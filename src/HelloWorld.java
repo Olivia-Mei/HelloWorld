cb8086ca7189ff5a5b128400b6c19ee3c51a6079
Olivia-Mei <Olivia-Mei@DESKTOP-UHLJ9IG.lan>
2021-02-09 15:43:37 -0600

    First commit...

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+// File: HelloWorld.java
+// Author: Olivia-Mei McDowell
+// Purpose: First Java Program
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello UMGC!");
+
+	}
+
+}
